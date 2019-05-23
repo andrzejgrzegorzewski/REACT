3f3b1a47b109cce9d9096622d415469cb882ad93
Andrzej Grzegorzewski <andrzej.grzegorzewski@gmail.com>
2019-05-23 23:43:39 +0200

    last finish

--- 01 - PROJECTS/project_03 - ShoppingCart/task_03 - ShoppingCart - Graying out of 0 in CSS/app.js
+++ 01 - PROJECTS/project_03 - ShoppingCart/task_03 - ShoppingCart - Graying out of 0 in CSS/app.js
@@ -1,45 +1,65 @@
 class App extends React.Component {
   state = {
     availableProducts: 8,
-    shoppingCart: 1,
-  }
+    shoppingCart: 1
+  };
 
   handleRemoveFromCart = () => {
     this.setState({
-      shoppingCart: this.state.shoppingCart - 1,
-    })
-  }
+      shoppingCart: this.state.shoppingCart - 1
+    });
+  };
 
   handleAddToCart = () => {
     this.setState({
-      shoppingCart: this.state.shoppingCart + 1,
-    })
-  }
+      shoppingCart: this.state.shoppingCart + 1
+    });
+  };
 
   handleBuy = () => {
     this.setState({
       availableProducts: this.state.availableProducts - this.state.shoppingCart,
-      shoppingCart: 0,
-    })
-  }
+      shoppingCart: 0
+    });
+  };
 
   render() {
+    const { shoppingCart, availableProducts } = this.state;
 
-    const {shoppingCart, availableProducts} = this.state
-
-    return ( 
+    return (
       <div>
-      <button disabled = {shoppingCart ? false : true} onClick = {this.handleRemoveFromCart}> - </button> 
-
-      <span style={shoppingCart === 0 ? {opacity: 0.3}:{color:"red    "}}> {shoppingCart} </span>
-
-      <button disabled = {shoppingCart === availableProducts ? true : false}
-      onClick = {this.handleAddToCart} > + </button> 
-      
-      {shoppingCart > 0 && <button onClick = {this.handleBuy} > Buy </button>}
+        <button
+          disabled={shoppingCart ? false : true}
+          onClick={this.handleRemoveFromCart}
+        >
+          {" "}
+          -{" "}
+        </button>
+        <span
+          style={
+            shoppingCart === 0
+              ? { opacity: 0.3 }
+              : {
+                  color: "red    "
+                }
+          }
+        >
+          {" "}
+          {shoppingCart}{" "}
+        </span>
+        <button
+          disabled={shoppingCart === availableProducts ? true : false}
+          onClick={this.handleAddToCart}
+        >
+          {" "}
+          +{" "}
+        </button>
+        {shoppingCart > 0 && (
+          <button onClick={this.handleBuy}> Buy </button>
+        )}{" "}
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render( < App / > , document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
